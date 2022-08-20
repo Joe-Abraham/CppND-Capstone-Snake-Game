481c6945ca0187f5fcbeac079e17ba4cd19abfa9
Joe Abraham <mail.joeabraham@gmail.com>
2022-08-20 09:39:20 +0530

    barebone structure of obstacle class

---
+++ src/obstacles.cpp
@@ -0,0 +1,51 @@
+#include "obstacles.h"
+#include <cmath>
+#include <iostream>
+
+void Obstacles::Update()
+{
+  UpdateHead();
+
+  // Update all of the points vector items if the obstacles head has moved to a new
+  // cell.
+}
+
+void Obstacles::UpdateHead()
+{
+  // Wrap the Obstacles around to the beginning if going off of the screen.
+  ;
+}
+
+void Obstacles::UpdateBody(SDL_Point &current_head_cell,
+                           SDL_Point &prev_head_cell)
+{
+  // Add previous head location to vector
+  points.push_back(prev_head_cell);
+
+  // Check if the obstacles has died.
+  for (auto const &item : points)
+  {
+    if (current_head_cell.x == item.x && current_head_cell.y == item.y)
+    {
+      snake_alive = false;
+    }
+  }
+}
+
+void Obstacles::IncreaseObstacles()
+{
+  ;
+}
+
+// Inefficient method to check if cell is occupied by obstacles.
+bool Obstacles::ObstaclesCell(int x, int y)
+{
+  for (auto const &item : points)
+  {
+    if (x == item.x && y == item.y)
+    {
+      return true;
+    }
+  }
+  return false;
+}
---
+++ src/obstacles.h
@@ -0,0 +1,30 @@
+#ifndef OBSTACLES_H
+#define OBSTACLES_H
+
+#include "SDL.h"
+#include <vector>
+
+class Obstacles
+{
+public:
+  Obstacles(int grid_width, int grid_height)
+      : grid_width(grid_width), grid_height(grid_height) {}
+
+  void Update();
+
+  void IncreaseObstacles();
+  bool ObstaclesCell(int x, int y);
+
+  int size{1};
+  bool snake_alive{true};
+  std::vector<SDL_Point> points;
+
+private:
+  void UpdateHead();
+  void UpdateBody(SDL_Point &current_cell, SDL_Point &prev_cell);
+
+  int grid_width;
+  int grid_height;
+};
+
+#endif
