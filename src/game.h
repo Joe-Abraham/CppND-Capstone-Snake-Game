0d6d86ff9423f014a1c484100d552ccf2d253498
Joe Abraham <mail.joeabraham@gmail.com>
2022-08-20 10:41:04 +0530

    Add function for collision.

--- src/game.h
+++ src/game.h
@@ -5,19 +5,23 @@
 #include "controller.h"
 #include "renderer.h"
 #include "snake.h"
+#include "obstacles.h"
 #include <random>
 
-class Game {
+class Game
+{
 public:
   Game(std::size_t grid_width, std::size_t grid_height);
   void Run(Controller const &controller, Renderer &renderer,
            std::size_t target_frame_duration);
   int GetScore() const;
   int GetSize() const;
+  void StuckObstacle(void);
 
 private:
   Snake snake;
   SDL_Point food;
+  Obstacles obs;
 
   std::random_device dev;
   std::mt19937 engine;
